58f5d4ab592b9e8fcdb916dcf9206e720c503af6
Paulina Korpak <korpak.paulina@gmail.com>
2021-06-11 22:47:01 +0100

    Render booking summary

--- src/features/booking/components/SeatsSummary/index.js
+++ src/features/booking/components/SeatsSummary/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { selectBookedSeats } from '../../bookingSlice';
 
 function SeatsSummary() {
+  const bookedSeats = useSelector(selectBookedSeats);
+
   return (
     <>
       <h4 className="text-center text-uppercase text-secondary">wybrane miejsca:</h4>
       <ListGroup className="mt-2 w-75">
-        <ListGroup.Item variant="dark" className="p-2 d-flex justify-content-around">
-          RZĄD
-          <strong> x1 </strong>
-          MIEJSCE
-          <strong> y1 </strong>
-        </ListGroup.Item>
-        <ListGroup.Item variant="dark" className="p-2 d-flex justify-content-around">
-          RZĄD
-          <strong> x1 </strong>
-          MIEJSCE
-          <strong> y1 </strong>
-        </ListGroup.Item>
-        <ListGroup.Item variant="dark" className="p-2 d-flex justify-content-around">
-          RZĄD
-          <strong> x1 </strong>
-          MIEJSCE
-          <strong> y1 </strong>
-        </ListGroup.Item>
+        {
+          bookedSeats.map((seat) => (
+            <ListGroup.Item
+              key={seat.id}
+              variant="dark"
+              className="p-2 d-flex justify-content-around"
+            >
+              RZĄD
+              <strong>{seat.cords.y}</strong>
+              MIEJSCE
+              <strong>{seat.cords.x}</strong>
+            </ListGroup.Item>
+          ))
+        }
       </ListGroup>
     </>
   );
